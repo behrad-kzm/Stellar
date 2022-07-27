92911332d093091e71ab358eb4c73646bf03265f
Behrad Kazemi <behrad.kzm@gmail.com>
2022-07-27 13:51:20 +0430

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@
 //LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 //OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //SOFTWARE.
-
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(name: "Stellar")
