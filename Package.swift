87a1ced0c2de3d8e306da8b987ae14536407d225
Behrad Kazemi <behrad.kzm@gmail.com>
2022-07-27 14:32:57 +0430

    Update Package.swift
    Fix swift package

--- Package.swift
+++ Package.swift
@@ -3,4 +3,21 @@
 
 import PackageDescription
 
-let package = Package(name: "Stellar")
+let package = Package(
+  name: "Stellar",   platforms: [
+    .macOS(.v10_12), .iOS(.v10), .tvOS(.v10), .watchOS(.v3)
+  ],
+  products: [
+    // Products define the executables and libraries a package produces, and make them visible to other packages.
+    .library(
+      name: "Stellar",
+      targets: ["Stellar"]),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages this package depends on.
+    .target(
+      name: "Stellar",
+      dependencies: []),
+  ]
+)
